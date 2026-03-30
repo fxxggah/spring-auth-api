231c45e8426dece32f222781314d636a79338f7e
Gabriel Oliveira <00001106883950sp@gmail.com>
2026-03-30 10:42:01 -0300

    refactor: adiciona mais metodos no JwtService

--- src/main/java/br/com/oliveira/auth_api/security/JwtService.java
+++ src/main/java/br/com/oliveira/auth_api/security/JwtService.java
@@ -1,12 +1,16 @@
 package br.com.oliveira.auth_api.security;
 
 import br.com.oliveira.auth_api.user.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -27,6 +31,44 @@ public class JwtService {
     private Key getSignKey() {
         byte[] keyBytes = SECRET_KEY.getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+
+        final Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts
+                .parser()
+                .verifyWith((SecretKey) getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+
+        final String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
     }
 
 }
