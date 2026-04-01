f4e8808f5657978fa2df4d98f4c526e79b657017
fxxggah <00001106883950sp@gmail.com>
2026-04-01 14:04:57 -0300

    refactor(request): adiciona validação de DTOs

--- src/main/java/br/com/oliveira/auth_api/dto/request/RegisterRequest.java
+++ src/main/java/br/com/oliveira/auth_api/dto/request/RegisterRequest.java
@@ -1,5 +1,11 @@
 package br.com.oliveira.auth_api.dto.request;
 
+
+import br.com.oliveira.auth_api.user.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -8,8 +14,19 @@ import lombok.*;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "Nome obrigatório")
+    @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
     private String name;
+
+    @NotBlank(message = "Email obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotBlank(message = "Senha obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
     private String password;
+
+    @NotNull(message = "Tipo de usuário obrigatório")
+    private UserType userType;
 
 }
